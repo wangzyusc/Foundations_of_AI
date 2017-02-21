51b5563f5bd83067cb99aa22bd83fbe9113d083c
wangzyusc <wangzhiy@usc.edu>
2017-02-20 17:38:52 -0800

    HW1 final version

--- HW1/alphaBetaPruning.py
+++ HW1/alphaBetaPruning.py
@@ -26,21 +26,14 @@ def isValidMove(board, pos, party):
     if board[y][x] != 0:
         return False
     opp = 3 - party
-    # print "opp is " + str(opp)
     vectors = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
     for vec in vectors:
-        # print "vec is " + str(vec)
-        # dst = pos + vec
         dst = [pos[0]+vec[0], pos[1]+vec[1]]
         count = 0
-        # print "dst is " + str(dst) + " dst[0] = " + str(dst[0]) + " dst[1] = " + str(dst[1])
         while isInBound(dst) and board[dst[0]][dst[1]] == opp:
-            #print "dst is " + str(dst)
-            # dst = dst + vec
             dst = [dst[0]+vec[0], dst[1]+vec[1]]
             count = count + 1
         if count > 0 and isInBound(dst) and board[dst[0]][dst[1]] == party:
-            #print "board[" + str(dst[0]) + "][" + str(dst[1]) + "] = " + str(party)
             return True
     return False
 
@@ -57,9 +50,6 @@ def terminalTest(board):
             elif board[y][x] == 2:
                 numX = numX + 1
             else:
-                # pos = list()
-                # pos.append(y)
-                # pos.append(x)
                 pos = [y, x]
                 if validMoveO == False and isValidMove(board,pos,1):
                     validMoveO = True
@@ -175,25 +165,19 @@ def maxvalue(board, alpha, beta, side, curDepth, name):
         print currActions
     for action in currActions:
         info = updateLog(name, curDepth, val, alpha, beta)
-        # print info
         if action == [-1, -1]:
             next_name = "pass"
         else:
             next_name = alphaNumericTable[action[1]] + str(action[0] + 1)
-        # val = max(val, minvalue(updateState(board, action, side), alpha, beta, side, curDepth + 1, next_name))
         score, move = minvalue(updateState(board, action, side), alpha, beta, side, curDepth + 1, next_name)
         if score > val:
-            #print "update: val from " + str(val) + " to " + str(score) + ", best move from " + str(
-            #    best_move) + " to " + str(move)
             val = score
             best_move = action
         if val >= beta:
             info = updateLog(name, curDepth, val, alpha, beta)
-            # print info
             return val, best_move
         alpha = max(alpha, val)
     info = updateLog(name, curDepth, val, alpha, beta)
-    # print info
     return val, best_move
 
 # minimize the score of side by choosing best move for opponent
@@ -208,7 +192,6 @@ def minvalue(board, alpha, beta, side, curDepth, name):
     if straightPasses == 2 or curDepth == search_depth:
         val = utility(board, side)
         info = updateLog(name, curDepth, val, alpha, beta)
-        # print info
         return val, best_move
     val = inf
     opp = 3 - side
@@ -227,47 +210,27 @@ def minvalue(board, alpha, beta, side, curDepth, name):
             next_name = "pass"
         else:
             next_name = alphaNumericTable[action[1]] + str(action[0] + 1)
-        # val = min(val, maxvalue(updateState(board, action, opp), alpha, beta, side, curDepth + 1, next_name))
         score, move = maxvalue(updateState(board, action, opp), alpha, beta, side, curDepth + 1, next_name)
         if score < val:
-            #print "update: val from " + str(val) + " to " + str(score) + ", best move from " + str(
-            #    best_move) + " to " + str(move)
             val = score
             best_move = action
         if val <= alpha:
             info = updateLog(name, curDepth, val, alpha, beta)
-            # print info
             return val, best_move
         beta = min(beta, val)
     info = updateLog(name, curDepth, val, alpha, beta)
-    # print info
     return val, best_move
-
-# legacy method, choose the best method with certain value
-def bestAction(board, actions, val, side):
-    if debugMode:
-        print actions
-    for move in actions:
-        newBoard = updateState(board, move, side)
-        score = utility(newBoard, side)
-        if True:#debugMode:
-            print "for " + str(move) + ", the score is " + str(score)
-        if score == val:
-            return move
-    return [-1, -1]
 
 # the method implementing Alpha-Beta Search
 def AlphaBetaSearch(board, side, depth):
     actions = getActions(board, side)
-    alpha = -inf # -1000000000 # -math.inf
-    beta = inf # 1000000000 # math.inf
+    alpha = -inf
+    beta = inf
     best_move = [-1, -1]
-    #print "Node,Depth,Value,Alpha,Beta"
     val, best_move = maxvalue(board, alpha, beta, side, depth, "root")
     if debugMode:
         print "maxvalue is " + str(val)
         print "actions are " + str(actions)
-    #return bestAction(board, actions, val, side)
     return best_move
 
 if __name__ == "__main__":
@@ -284,28 +247,23 @@ if __name__ == "__main__":
     board = list()
     with open(inputfilepath) as inputf:
         for i, line in enumerate(inputf):
-            # print str(i) + " " + line
             if i == 0:
                 player = str(line)[0]
             elif i == 1:
                 search_depth = int(line)
             else:
                 init_board.append(line)
-                # board = board + str(line)
     if debugMode:
         print "Player is " + player
         print "depth is " + str(search_depth)
-    # print "board is: \n" + board
     party_table = ['*', 'O', 'X']
     party = party_table.index(player)
-    # print "party is " + str(party)
     for line in init_board:
         if debugMode:
             print line[:8]
         row = list()
         for i in xrange(0, 8):
             row.append(party_table.index(line[i]))
-        # print str(row)
         board.append(row)
 
     if debugMode:
@@ -320,21 +278,16 @@ if __name__ == "__main__":
                 string = string + str(isValidMove(board, pos, 2)) + " "
                 print string
 
-    #print "Test AlphaBetaSearch: "
     tmpParty = party
     alphaNumericTable = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     step = AlphaBetaSearch(board, party, 0)
-    #print alphaNumericTable[step[1]] + str(step[0] + 1)
-    #print step
     next_board = updateState(board, step, tmpParty)
 
     with open(outputfilepath, 'w') as outputf:
-        #outputf.write('Hello World!\n')
         for row in next_board:
             line = ""
             for pixel in row:
                 line = line + party_table[pixel]
-            #print line
             line = line + "\n"
             outputf.write(line)
         traverseLog = traverseLog[:len(traverseLog)-1]
